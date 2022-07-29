18f2ac8d568c42376f2a803da98ee7e6b112d430
61456421 <61068710+61456421@users.noreply.github.com>
2022-07-29 18:24:21 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitea.com/61456421/1/raw/branch/master/jkw.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
